e782982a6a897e447ebc3b4ec8c85a844f9425b9
scroco158 <scroco@mail.ru>
2023-12-16 21:25:00 +0300

    DZ 1

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,14 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
+
+
+def test_calculate_total_price():
+    it1 = Item("it1", 100, 10)
+    assert it1.calculate_total_price() == 1000
+
+
+def test_apply_discount():
+
+    it1 = Item("it1", 100, 10)
+    it1.apply_discount()
+    assert it1.price == 110
